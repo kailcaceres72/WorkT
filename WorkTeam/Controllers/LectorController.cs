f36f5a9aa85b03deb0fc77bf66d17b87bdcc7375
joan david pichardo hernandez <joanmejor001@hotmail.com>
2019-11-15 14:43:51 -0500

    Lector funcional y codigo de consulta comentado

---
+++ WorkTeam/Controllers/LectorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkTeam.Models;
+
+namespace WorkTeam.Controllers
+{
+    public class LectorController : Controller
+    {
+        // GET: Lector
+        public ActionResult Index()
+        {
+            return View(new List<Customer_Lector>());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            List<Customer_Lector> customers = new List<Customer_Lector>();
+            string filePath = string.Empty;
+            if (postedFile != null)
+            {
+
+                string path = Server.MapPath("/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                filePath = path + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+                foreach (String row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        customers.Add(new Customer_Lector
+                        {
+
+                            Id = row.Split(',')[0],
+                            Nombre = row.Split(',')[1],
+                            Pais = row.Split(',')[2]
+                        });
+                    }
+                }
+            }
+            return View(customers);
+        }
+    }
+}
---
+++ WorkTeam/Models/Customer_Lector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkTeam.Models
+{
+    public class Customer_Lector
+    {
+        public String Id { get; set; }
+        public String Nombre { get; set; }
+        public String Pais { get; set; }
+
+    }
+}
